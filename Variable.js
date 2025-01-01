542480dad3ec0e22037a1750b7c4742ffd16bab8
Lokesh Khankari <159478330+Lokesh7757@users.noreply.github.com>
2025-01-01 12:39:36 +0530

    Add files via upload

---
+++ Variable.js
@@ -0,0 +1,25 @@
+// ** Var | let | Const **
+
+// Var - Global
+// Re-Assign 
+// I can Access AnyWhere
+
+
+//let - Local
+// Re-Assign
+// I can Access Only With In Block
+
+
+// Const - Local 
+// Re-Assign Cant
+// I can Access Only With In Block
+
+
+var age = 22;
+console.log(age)
+
+let a = 20
+console.log(a)
+
+const Name = "lokesh"
+console.log(Name)
