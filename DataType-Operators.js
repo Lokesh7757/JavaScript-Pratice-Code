ab8397330d5b8d6f54bb9d4c4d2a07a639395f37
Lokesh Khankari <159478330+Lokesh7757@users.noreply.github.com>
2025-01-02 17:21:09 +0530

    Add files via upload

---
+++ DataType-Operators.js
@@ -0,0 +1,41 @@
+// DataTypes
+// 1.Numbers 2.Strings 3.Booleans 4.NULL 5.Undefined 6.Objects 
+
+// Operators 
+// +, -, *, / 
+
+
+// 1.Numbers 
+let num = 3;
+console.log(num)
+
+// 2.Strings
+const firstName = "Lokesh"
+const lastName = "Khankari"
+console.log(firstName + ' ' + lastName)
+
+// 3. Booleans
+let isLoggedIn = true
+console.log(isLoggedIn)
+
+// 4. NULL
+let lastLoginDate = null;
+console.log(lastLoginDate)
+
+// 5. Objects
+const person = {
+    firstName: 'Lokesh',
+    lastName: 'Khankari',
+    age: 22,
+    isLoggedIn: true
+}
+console.log(person.age)
+
+
+// Operators 
+let num1 = 10;
+let num2 = 20;
+console.log(num1 + num2)
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
