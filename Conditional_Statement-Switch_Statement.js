19e8033dbdc5f905f1c5d73973e267397165c22f
Lokesh Khankari <159478330+Lokesh7757@users.noreply.github.com>
2025-01-03 11:01:08 +0530

    Add files via upload

---
+++ Conditional_Statement-Switch_Statement.js
@@ -0,0 +1,23 @@
+// Conditional_Statement
+
+const age = 20;
+if (age >= 18){
+    console.log('Yes, You can Vote')
+}
+else {
+    console.log('No, you cant Vote')
+}
+
+// Switch_Statement
+
+const option = 2;
+switch(option) {
+    case 1: console.log('Namaste')
+    break;
+    case 2: console.log('Hello')
+    break;
+    case 3: console.log('World')
+    break;
+
+    default: console.log('Invalid Option')
+}
