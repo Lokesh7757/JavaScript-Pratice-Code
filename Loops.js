f1101e371bc1dbd7f1c29c76f091d4c08d053c35
Lokesh Khankari <159478330+Lokesh7757@users.noreply.github.com>
2025-01-08 19:29:36 +0530

    Add files via upload

---
+++ Loops.js
@@ -0,0 +1,22 @@
+// Loops 
+
+// For Loop -> If You Konw how Many Times
+for(let i=1; i<=10000; i=i+1){
+    console.log('Lokesh')
+}
+
+// While Loop -> 
+let ip = 0;
+let house = 100
+
+while (ip != 100){
+    ip = ip + 1;
+    console.log('Step Taken' + ip)
+}
+
+// Do While 
+do {
+    ip = ip + 1;
+    console.log('Step Taken' + ip)
+    while (ip <= house)
+}
