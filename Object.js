19320700c667215da8297988860fbc2b1bd2d0f7
Lokesh Khankari <159478330+Lokesh7757@users.noreply.github.com>
2025-02-28 14:56:00 +0530

    Add files via upload

---
+++ Object.js
@@ -0,0 +1,15 @@
+// object
+
+const User = {
+    name: "Lokesh",
+    age : 22,
+    email: "lokesh@gmail.com",
+    location: "Nashik"
+}
+
+console.log(User.location);
+
+
+// object destructuring
+
+const course = 
